f4f4a90008a692ba39d7bce78c4ba78997cd0045
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-10 17:46:25 -0400

    Finish Preliminary CSS

---
+++ assets/javascript/script.js
@@ -0,0 +1,20 @@
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyAmiG9z0HqSfUE_VqLZYXtmu6MyncZAZ94",
+    authDomain: "trainscheduler-cd762.firebaseapp.com",
+    databaseURL: "https://trainscheduler-cd762.firebaseio.com",
+    projectId: "trainscheduler-cd762",
+    storageBucket: "",
+    messagingSenderId: "731094045454"
+  };
+  firebase.initializeApp(config);
+
+
+
+// Input fields need to be pushed to server 
+
+// server needs to be pulled back to website 
+
+// Moment.js calculations 
+
+// Properties need to be displayed on page
