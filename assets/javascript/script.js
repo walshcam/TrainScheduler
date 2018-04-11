18d81f64789f387434aef23e4762e92b11afa759
walshcam <35083171+walshcam@users.noreply.github.com>
2018-04-10 20:08:24 -0400

    Add moment.js to folder

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -9,9 +9,52 @@
   };
   firebase.initializeApp(config);
 
+  //database variable
+
+  let database = firebase.database();
+
+// Input fields need to be pushed to server on button click
+
+$("#findButton").on("click",function(event) {
+    //prevents user from clicking button without any input fields?
+    event.preventDefault();
+
+    // Get user input
+    let name = $("#name").val().trim();
+    let destination = $("destination").val().trim();
+    let time = $("firstTrainTime").val().trim();
+    let frequency = $("frequency").val().trim();
+
+    //Create object for holding train data
+
+    let newTrain = {
+        name: name,
+        destination: destination,
+        time: time,
+        frequency: frequency
+    }
+
+    //Upload employee data to database
+    //set replaces data while push adds new data
+
+    database.ref().push(newTrain);
+
+    //log everything to the console
+
+    console.log(newTrain.name);
+    console.log(newTrain.destination);
+    console.log(newTrain.time);
+    console.log(newTrain.frequency);
+
+    //Clear all text boxes for new info
+
+    $("#name").val().trim("");
+    $("destination").val().trim("");
+    $("firstTrainTime").val().trim("");
+    $("frequency").val().trim("");
 
 
-// Input fields need to be pushed to server 
+});
 
 // server needs to be pulled back to website 
 
